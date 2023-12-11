84987e2325274375faac0302d8d0563be0b8ae2a
Nigel Thorne <nigelt@dasintel.io>
2023-12-11 11:44:10 +1100

    reformat

--- src/deferred_gdal_writer.cpp
+++ src/deferred_gdal_writer.cpp
@@ -14,8 +14,7 @@
 #include "deferred_gdal_writer.h"
 #include "operation.h"
 
-namespace exactextract
-{
+namespace exactextract {
 
     void
     DeferredGDALWriter::write(const std::string &fid)
@@ -42,101 +41,75 @@ namespace exactextract
         m_ops.push_back(&op);
     }
 
-    void
-    DeferredGDALWriter::finish()
-    {
-        std::map<std::string, OGRFieldDefnH> ogr_fields;
+void
+DeferredGDALWriter::finish()
+{
+    std::map<std::string, OGRFieldDefnH> ogr_fields;
 
-        for (const auto &feature : m_features)
-        {
-            for (const auto &[field_name, value] : feature.map())
-            {
+    for (const auto& feature : m_features) {
+        for (const auto& [field_name, value] : feature.map()) {
 
-                if (ogr_fields.find(field_name) == ogr_fields.end())
-                {
-                    OGRFieldType field_type;
+            if (ogr_fields.find(field_name) == ogr_fields.end()) {
+                OGRFieldType field_type;
 
-                    if (value.type() == typeid(std::string))
-                    {
-                        field_type = OFTString;
-                    }
-                    else if (value.type() == typeid(double) || value.type() == typeid(float))
-                    {
-                        field_type = OFTReal;
-                    }
-                    else if (value.type() == typeid(std::int64_t) || value.type() == typeid(std::uint64_t))
-                    {
-                        field_type = OFTInteger64;
-                    }
-
-                    ogr_fields[field_name] = OGR_Fld_Create(field_name.c_str(), field_type);
+                if (value.type() == typeid(std::string)) {
+                    field_type = OFTString;
+                } else if (value.type() == typeid(double) || value.type() == typeid(float)) {
+                    field_type = OFTReal;
+                } else if (value.type() == typeid(std::int64_t) || value.type() == typeid(std::uint64_t)) {
+                    field_type = OFTInteger64;
                 }
+
+                ogr_fields[field_name] = OGR_Fld_Create(field_name.c_str(), field_type);
             }
-        }
-
-        constexpr bool approx_ok = true;
-
-        for (const auto &[field_name, field_defn] : ogr_fields)
-        {
-            OGRFeatureDefnH defn = OGR_L_GetLayerDefn(m_layer);
-
-            if (OGR_FD_GetFieldIndex(defn, field_name.c_str()) == -1)
-            {
-                OGR_L_CreateField(m_layer, field_defn, approx_ok);
-            }
-        }
-
-        OGRFeatureDefnH defn = OGR_L_GetLayerDefn(m_layer);
-        for (const auto &feature : m_features)
-        {
-            OGRFeatureH f = OGR_F_Create(defn);
-
-            for (const auto &[field_name, value] : feature.map())
-            {
-                auto pos = OGR_F_GetFieldIndex(f, field_name.c_str());
-
-                if (pos == -1)
-                {
-                    throw std::runtime_error("Unexpected field: " + field_name);
-                }
-
-                if (value.type() == typeid(std::string))
-                {
-                    OGR_F_SetFieldString(f, pos, std::any_cast<std::string>(value).c_str());
-                }
-                else if (value.type() == typeid(float))
-                {
-                    OGR_F_SetFieldDouble(f, pos, static_cast<double>(std::any_cast<float>(value)));
-                }
-                else if (value.type() == typeid(double))
-                {
-                    OGR_F_SetFieldDouble(f, pos, std::any_cast<double>(value));
-                }
-                else if (value.type() == typeid(std::int64_t))
-                {
-                    OGR_F_SetFieldInteger64(f, pos, std::any_cast<std::int64_t>(value));
-                }
-                else if (value.type() == typeid(std::uint64_t))
-                {
-                    auto intval = std::any_cast<std::uint64_t>(value);
-                    if (intval > std::numeric_limits<std::int64_t>::max())
-                    {
-                        throw std::runtime_error("Unhandled int64 value.");
-                    }
-                    OGR_F_SetFieldInteger64(f, pos, static_cast<std::int64_t>(intval));
-                }
-                else
-                {
-                    throw std::runtime_error("Unhandled type: " + std::string(value.type().name()));
-                }
-            }
-
-            if (OGR_L_CreateFeature(m_layer, f) != OGRERR_NONE)
-            {
-                throw std::runtime_error("Error writing feature.");
-            }
-            OGR_F_Destroy(f);
         }
     }
+
+    constexpr bool approx_ok = true;
+
+    for (const auto& [field_name, field_defn] : ogr_fields) {
+        OGRFeatureDefnH defn = OGR_L_GetLayerDefn(m_layer);
+
+        if (OGR_FD_GetFieldIndex(defn, field_name.c_str()) == -1) {
+            OGR_L_CreateField(m_layer, field_defn, approx_ok);
+        }
+    }
+
+    OGRFeatureDefnH defn = OGR_L_GetLayerDefn(m_layer);
+    for (const auto& feature : m_features) {
+        OGRFeatureH f = OGR_F_Create(defn);
+
+        for (const auto& [field_name, value] : feature.map()) {
+            auto pos = OGR_F_GetFieldIndex(f, field_name.c_str());
+
+            if (pos == -1) {
+                throw std::runtime_error("Unexpected field: " + field_name);
+            }
+
+            if (value.type() == typeid(std::string)) {
+                OGR_F_SetFieldString(f, pos, std::any_cast<std::string>(value).c_str());
+            } else if (value.type() == typeid(float)) {
+                OGR_F_SetFieldDouble(f, pos, static_cast<double>(std::any_cast<float>(value)));
+            } else if (value.type() == typeid(double)) {
+                OGR_F_SetFieldDouble(f, pos, std::any_cast<double>(value));
+            } else if (value.type() == typeid(std::int64_t)) {
+                OGR_F_SetFieldInteger64(f, pos, std::any_cast<std::int64_t>(value));
+            } else if (value.type() == typeid(std::uint64_t)) {
+                auto intval = std::any_cast<std::uint64_t>(value);
+                if (intval > std::numeric_limits<std::int64_t>::max()) {
+                    throw std::runtime_error("Unhandled int64 value.");
+                }
+                OGR_F_SetFieldInteger64(f, pos, static_cast<std::int64_t>(intval));
+            } else {
+                throw std::runtime_error("Unhandled type: " + std::string(value.type().name()));
+            }
+        }
+
+        if (OGR_L_CreateFeature(m_layer, f) != OGRERR_NONE) {
+            throw std::runtime_error("Error writing feature.");
+        }
+        OGR_F_Destroy(f);
+    }
+}
 
 }
